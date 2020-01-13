ea7ec30986f622e152c94358fe5e36d1bc977ab2
Debfront <deborah_monteiro12@hotmail.com>
2020-01-13 20:18:05 -0300

    13/jan

--- _atualizar_categoria.php
+++ _atualizar_categoria.php
@@ -12,7 +12,7 @@ $atualizar = mysqli_query($conexao, $sql);
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <div class="container" style="width: 400px">
 <center>
-<h3>Atualizado com sucesso</h3>
+<h3>Atualizado com sucesso!</h3>
 <div style="margin-top:10px">
 <a href="listar_categoria.php" class= "btn btn-sm btn-warning" style="color:#fff">Voltar</a>
 </div>
