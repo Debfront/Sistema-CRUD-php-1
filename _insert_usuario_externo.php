6b6d8839f136020d004e9c660d821332835f8028
Debfront <deborah_monteiro12@hotmail.com>
2020-01-13 20:14:06 -0300

    13/jan

--- _insert_usuario_externo.php
+++ _insert_usuario_externo.php
@@ -17,7 +17,7 @@ $inserir = mysqli_query($conexao, $sql);
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <div class="container" style="width: 400px">
 <center>
-<h3>Usuário cadastrado com sucesso, esperando aprovação</h3>
+<h3>Usuário cadastrado com sucesso, esperando aprovação!</h3>
 <div style="margin-top:10px">
 <a href="cadastro_usuario.php" class= "btn btn-sm btn-warning" style="color:#fff">Voltar</a>
 </div>
