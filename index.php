efccefa8ca9a14f23cb5ffa57320afba2c306576
Debfront <deborah_monteiro12@hotmail.com>
2020-01-13 20:04:58 -0300

    13/jan

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-	//Se já tiver logado, não precisa logar novamente
+	//Se já tiver logado, não precisa logar novamente.
 	if (isset($_COOKIE['user'])) {
          header("Location: menu.php");
   	}
@@ -19,9 +19,7 @@
 			   -webkit-box-shadow: 10px 10px 26px -4px rgba(181,181,181,1);
 			   -moz-box-shadow: 10px 10px 26px -4px rgba(181,181,181,1);
 				box-shadow: 10px 10px 26px -4px rgba(181,181,181,1);
-
-
-		   }
+		    }
 		
 		</style>
 	</head>
@@ -55,11 +53,7 @@
 		</center>
 	</div>
 
-
-
-
-
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
